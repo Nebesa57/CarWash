6ca9de314b6dbe0addbe2167c4c8c9ba445417bc
Dan NENM <nebesa577@gmail.com>
2023-04-14 22:12:49 +0300

    1

--- src/main/java/com/example/CarWash/controllers/RecordsController.java
+++ src/main/java/com/example/CarWash/controllers/RecordsController.java
@@ -7,11 +7,10 @@ import com.example.CarWash.dto.UserDto;
 import com.example.CarWash.models.Records;
 import com.example.CarWash.models.User;
 import com.example.CarWash.repository.RecordsRepository;
+import com.example.CarWash.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
@@ -24,11 +23,22 @@ import java.util.stream.Collectors;
 public class RecordsController {
     private final RecordsMapper recordsMapper;
     private final RecordsRepository recordsRepository;
+    private final UserService userService;
 
     @GetMapping("recordsAll")
     public List<RecordsDto> getRecords() {
         try {
             return recordsRepository.findAll().stream().map(el->recordsMapper.toDTOList(el)).collect(Collectors.toList());
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong", e);
+        }
+    }
+
+    @PostMapping(value = "records/{id}")
+    public String createMessage(@RequestBody Records records, @PathVariable("id") Long id) {
+        try {
+            recordsRepository.save(new Records(records.getTime(),records.getDate(), userService.findById(id).get(),records.getService()));
+            return "Вы записались";
         } catch (Exception e) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Something went wrong", e);
         }
