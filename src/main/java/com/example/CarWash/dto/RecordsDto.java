594c5067f6947611a2b46c37db49a99860c978c5
Dan NENM <nebesa577@gmail.com>
2023-04-18 23:52:55 +0300

    1

--- src/main/java/com/example/CarWash/dto/RecordsDto.java
+++ src/main/java/com/example/CarWash/dto/RecordsDto.java
@@ -5,6 +5,7 @@ import lombok.Data;
 @Data
 public class RecordsDto {
     private UserDto userDto;
+    private Long id;
     private String service;
     private String date;
     private String startTime;
